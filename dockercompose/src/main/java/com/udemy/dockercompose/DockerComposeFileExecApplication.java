54d95eab6de762140ecc9f9f8ef203d4635f8662
Taher2011 <taher.jit@gmail.com>
2021-09-16 09:16:27 +0530

    Add Dockercompose project

---
+++ dockercompose/src/main/java/com/udemy/dockercompose/DockerComposeFileExecApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.dockercompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerComposeFileExecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerComposeFileExecApplication.class, args);
+	}
+
+}
---
+++ dockercompose/src/test/java/com/udemy/dockercompose/DockerComposeFileExecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.dockercompose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerComposeFileExecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
