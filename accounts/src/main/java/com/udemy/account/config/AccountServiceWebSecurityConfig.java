4ec8f9da2a4d3f4e0300e924a010e8ca4479c6f1
Taher2011 <taher.jit@gmail.com>
2021-09-19 20:09:01 +0530

    Rename

--- accounts/src/main/java/com/udemy/account/config/AccountServiceWebSecurityConfig.java
+++ accounts/src/main/java/com/udemy/account/config/AccountServiceWebSecurityConfig.java
@@ -5,7 +5,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class AccountServiceWebSecConfig extends WebSecurityConfigurerAdapter {
+public class AccountServiceWebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
